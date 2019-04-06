5267ad45789761b487149fd0ee26438b20f812e8
admin <admin@admins-MacBook-Pro.local>
2019-04-06 10:37:47 +0530

    add script to create and write in a file

---
+++ reading and writing file in python.py
@@ -0,0 +1,8 @@
+newfile = open("filebypython","w+") #this is to create a variable to store the file name and the operation that we will do on it
+
+write = "wow this text is written by python script "     #this is the text that we need to write into the file stored in a variable
+
+newfile.write(write)  # the newfile is the file and .write is the function that will write the file
+
+newfile.read()
+
