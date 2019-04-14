8ffb19fc390b9ee4a600d092403407981b82882a
admin <admin@admins-MacBook-Pro.local>
2019-04-14 10:52:35 +0530

    adding multgiple finds

---
+++ py_mongoTutorials/Multiple find conditions.py
@@ -0,0 +1,18 @@
+import pymongo
+from pymongo import MongoClient
+
+MyClient = MongoClient()    # again this is connecting to deault host and port
+
+db = MyClient.mydatabase #db is a variable to store the database my database
+
+Users = db.users   #this  is the table
+
+
+
+#like here we are finding the  document Name where name is raju
+
+
+
+Users.find()
+lastcount = Users.count_documents({"password": "gadha","Name": "biju"})
+print(lastcount)
